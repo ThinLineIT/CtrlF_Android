4c0d6eddee6a08ae34106f7d50c87e0f6e58992f
Donggi Lee <rkdmf1026@naver.com>
2021-10-07 22:02:17 +0900

    Refactor main toolbar
    프래그먼트 툴바 -> 액티비티 툴바 변경

--- app/src/main/java/com/thinlineit/ctrlf/MainActivity.kt
+++ app/src/main/java/com/thinlineit/ctrlf/MainActivity.kt
@@ -3,12 +3,41 @@ package com.thinlineit.ctrlf
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.thinlineit.ctrlf.databinding.ActivityMainBinding
+import com.thinlineit.ctrlf.logout.LogoutActivity
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.apply {
+            setSupportActionBar(toolBar)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.userCircleBtn -> {
+            val intent = Intent(this, LogoutActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     companion object {
