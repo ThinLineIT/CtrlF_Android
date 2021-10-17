ff06c1732f51cfc0b892628a705c9ee0deea63a1
minki_hong <gaogai2@naver.com>
2021-10-17 23:20:34 +0900

    issue in TopicTitleListFragment.kt of swipe

--- app/src/main/java/com/thinlineit/ctrlf/page/TopicTitleListFragment.kt
+++ app/src/main/java/com/thinlineit/ctrlf/page/TopicTitleListFragment.kt
@@ -11,6 +11,8 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.thinlineit.ctrlf.databinding.FragmentTopicTitleBinding
+import com.thinlineit.ctrlf.MainActivity.Companion.swipeWidth
+import kotlinx.android.synthetic.main.list_item_topic_title.topicTitleDeleteButton
 
 class TopicTitleListFragment : Fragment() {
     private val topicTitleListAdapter = TopicTitleListAdapter { topicId, topicTitle ->
@@ -19,7 +21,13 @@ class TopicTitleListFragment : Fragment() {
             TopicTitleListFragmentDirections.actionNotesFragmentToPageFragment()
         )
     }
-    private val itemTouchHelper = ItemTouchHelper(SwipeController(topicTitleListAdapter))
+
+    /*private val swipeHelperCallback = SwipeHelperCallback().apply {
+        val widthSwipe = topicTitleDeleteButton.height
+        setClamp(widthSwipe.toFloat())
+    }
+    private val itemTouchHelper = ItemTouchHelper(swipeHelperCallback)*/
+
     private val pageViewModel by activityViewModels<PageViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,11 +41,23 @@ class TopicTitleListFragment : Fragment() {
         ).apply {
             this.pageViewModel = this@TopicTitleListFragment.pageViewModel
             lifecycleOwner = this@TopicTitleListFragment
+
             topicListRecyclerView.adapter = topicTitleListAdapter
+
+            val swipeHelperCallback = SwipeHelperCallback().apply {
+                val widthSwipe = topicTitleDeleteButton.height
+                setClamp(widthSwipe.toFloat())
+            }
+            val itemTouchHelper = ItemTouchHelper(swipeHelperCallback)
+
             itemTouchHelper.attachToRecyclerView(topicListRecyclerView)
+
+
             topicListRecyclerView.layoutManager =
                 LinearLayoutManager(this@TopicTitleListFragment.context)
             // TODO: 툴바 이미지 변경, 클릭 시 준비중입니다 다이얼로그 적용
+            topicListRecyclerView.addItemDecoration(ItemDecoration())
+
             addTopicBtn.setOnClickListener { view ->
                 Toast.makeText(activity, "해당 서비스는 준비중입니다.", Toast.LENGTH_SHORT).show()
             }
